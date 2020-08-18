473bf144559a42f4a493954f3d15dc8e5c19f67f
prashwar <prashwarmishra@gmail.com>
2020-08-18 21:12:48 +0530

    fifth commit:Defined partials and layouts and Set up static file access

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const port = 8000;
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(express.static('assets'));
 app.use('/', require('./routes'));
 
 app.set('view engine', 'ejs');
