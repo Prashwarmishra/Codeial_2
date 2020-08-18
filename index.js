5c69bf654091ddef621872803bcfec89fd893e56
prashwar <prashwarmishra@gmail.com>
2020-08-18 16:28:47 +0530

    third commit: Install ejs and set up the view engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use('/', require('./routes'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if (err){console.log(`There's an error while connecting to the server: ${err}`); return;}
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
-router.get('/practise', homeController.practise);
-router.get('/profile', homeController.profile);
+
+router.use('/users', require('./users'));
+router.use('/users', require('./posts'));
 
 module.exports = router;
