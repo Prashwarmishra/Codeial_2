7bffa8f2eb0881fc65af464e446e8a2be158d162
prashwar <prashwarmishra@gmail.com>
2020-08-20 23:14:12 +0530

    commit 9: Install and implement SCSS and style the layout.

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const { setAuthenticatedUser } = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css'
+}));
 
 app.use(expressLayouts);
 
