8bb1bdf302f0da839210b8d19ff802184480a7f6
prashwar <prashwarmishra@gmail.com>
2020-08-21 17:13:00 +0530

    commit 12: Fetch and show the posts from the database on the home page and populate the user data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,22 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies)
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: 'Home',
+    // console.log(req.cookies)
+    // res.cookie('user_id', 25);
+    // Post.find({}, function(err, posts){
+    //     if (err){console.log("There's an error while fetching the Posts."); return;}
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         posts: posts
+    //     })
+    // })
+    
+    Post.find({}).populate('user').exec(function(err, posts){
+        if (err){console.log("There's an error while fetching the Posts."); return;}
+        return res.render('home', {
+            title: 'Home',
+            posts: posts
+        })
     })
 }
+
+
--- models/post.js
+++ models/post.js
@@ -9,6 +9,8 @@ const postSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
     }
+}, {
+    timestamps: true
 });
 
 const Post = mongoose.model('Post', postSchema);
