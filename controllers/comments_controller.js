4f6f76fd1e9eabe444e4d936f90cd1d28ad290b3
prashwar <prashwarmishra@gmail.com>
2020-08-22 15:29:04 +0530

    commit 16: Add a delete button for the comments in the Post section.

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const { post } = require('../routes/posts');
 
 module.exports.create = function(req, res){
     Post.findById(req.body.post, function(err, post){
@@ -16,6 +17,23 @@ module.exports.create = function(req, res){
 
                 return res.redirect('back');
             })
+        }
+    })
+};
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if (err){console.log("Error while deleting the Comment."); return;}
+        if (comment.user == req.user.id){
+            
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, {$pull : {comments: req.params.id}}, function(err, post){
+                if (err){console.log("There's an error while updating the Post Schema"); return;}
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
         }
     })
 }
