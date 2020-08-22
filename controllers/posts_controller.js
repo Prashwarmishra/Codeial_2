66ee4b9e1211242a5fa8da2491f0be205d87bdc8
prashwar <prashwarmishra@gmail.com>
2020-08-22 14:02:22 +0530

    commit 15: Add a delete button to the Post.

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -7,5 +8,27 @@ module.exports.create = function(req, res){
     }, function(err, post){
         if (err){console.log("There's an error while adding the Post"); return;}
         return res.redirect('back');
+    })
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        console.log(req.user.id);
+        console.log(post.user);
+        if (err) {console.log("There's an error while deleting the Post"); return;}
+
+        if (post.user == req.user.id){
+            
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if (err){
+                    console.log("There's an error while deleting the comment.");
+                    return;
+                }
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
     })
 }
