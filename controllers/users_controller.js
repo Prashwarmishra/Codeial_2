2e053860edad9b68696e802c6dbea9844eb6e816
prashwar <prashwarmishra@gmail.com>
2020-08-26 14:50:51 +0530

    commit 22: Install and configure multer to be able to store and upload media

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require("path");
 
 module.exports.profile = function(req, res){
     User.findById(req.params.id, function(err, user){
@@ -10,14 +12,40 @@ module.exports.profile = function(req, res){
     })
 }
 
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+    // if (req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         if (err){console.log("There's an error while updating Credentials."); return;}
+    //         return res.redirect('/');
+    //     });
+    // }else{
+    //     return res.status(401).send("Unauthorized.");
+    // }
     if (req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            if (err){console.log("There's an error while updating Credentials."); return;}
-            return res.redirect('/');
-        });
-    }else{
-        return res.status(401).send("Unauthorized.");
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if (err){
+                    console.log("Error handling multer"); return;
+                }
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){
+                    if (user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                req.flash("success", "Credentials changed Successfully!");
+                return res.redirect('back');
+            })
+        }catch(err){
+            req.flash("error", "There's an error while updating the User Credentials.");
+            return res.redirect('back');
+        }
     }
 }
 
