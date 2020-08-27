87f231077d9450d35b94a38d840df9c20330b300
prashwar <prashwarmishra@gmail.com>
2020-08-27 13:20:08 +0530

    commit 24: Finish building API for authentication using passport-jwt for interacting with frontend devices.

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -23,12 +23,18 @@ module.exports.destroy = async function(req, res){
     try{
         let post = await Post.findById(req.params.id);
 
-        post.remove();
-        await Comment.deleteMany({post: req.params.id});
+        if (post.user == req.user.id){
+            post.remove();
+            await Comment.deleteMany({post: req.params.id});
 
-        return res.json(200, {
-            message: "Post Deleted Successfully."
-        })
+            return res.json(200, {
+                message: "Post Deleted Successfully."
+            })
+        }else{
+            return res.json(401,{
+                message: "Unauthorized to delete the Post",
+            });
+        }
     }catch(err){
         console.log(err);
         return res.json(500, {
--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -12,7 +12,7 @@ module.exports.createSession = async function(req, res){
         return res.json(200,{
             message: "Here's your Key, keep it safely!",
             data: {
-                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '10000'})
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '100000'})
             }
         })
     }catch(err){
